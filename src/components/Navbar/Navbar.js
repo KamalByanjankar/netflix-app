502f647a67d898318629c29715ec8b1f487b4324
Kamal Byanjanakr <kamalbyanjankar@gmail.com>
2021-03-13 21:28:11 +0100

    Navbar edited

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,13 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useHistory } from 'react-router'
 import './Navbar.css'
 
 function Navbar() {
     const history = useHistory()
+    const [show, setShow] = useState(false)
 
+    const transitionNavbar = () => {
+        if(window.scrollY > 80){
+            setShow(true)
+        }
+        else{
+            setShow(false)
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener("scroll", transitionNavbar)
+        return () => {
+            window.removeEventListener("scroll", transitionNavbar)
+        }
+    }, [])
 
     return (
-        <div className="navbar">
+        <div className={`navbar ${show && "navbar__black"}`}>
             <div className="navbar__contents">
                 <img
                     src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
