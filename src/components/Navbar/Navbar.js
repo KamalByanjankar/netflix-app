dca7b313156960c97275a9fd98d82ee68bfd0b46
Kamal Byanjanakr <kamalbyanjankar@gmail.com>
2022-03-29 09:44:02 +0200

    title and routing changed

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -29,7 +29,7 @@ function Navbar() {
                     src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
                     alt="NETFLIX"
                     className="navbar__netflixlogo"
-                    onClick={() => history.push("/home")}
+                    onClick={() => history.push("/")}
                 />
 
                 <img 
